5967a03fa8901253f728eb8c25785048d28373ec
Jeff Li <a0983619822@gmail.com>
2023-06-03 19:40:20 +0800

    complete binary string addition

--- binary string addition.py
+++ binary string addition.py
@@ -1,3 +1,11 @@
 a=input()
 b=input()
-print(f"a={int(a,2)},b={int(b,2)},a+b={int(a,2)+int(b,2)}")
+print(f"a={int(a,2)},b={int(b,2)}")
+c = int(a,2) + int(b,2)
+binary_string = ""
+while c > 0:
+    remainder = c % 2
+    c = c // 2
+    binary_string = str(remainder) + binary_string
+    # print(f"remainder={remainder},c={c},binary_string={binary_string}")
+print(binary_string)
