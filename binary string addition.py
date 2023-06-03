98bf0e854cceeee1aff7a8c2909f2e47011621d2
Jeff Li <a0983619822@gmail.com>
2023-06-03 19:10:32 +0800

    add binary string addition

---
+++ binary string addition.py
@@ -0,0 +1,3 @@
+a=input()
+b=input()
+print(f"a={int(a,2)},b={int(b,2)},a+b={int(a,2)+int(b,2)}")
--- test.py
+++ test.py
@@ -1,5 +1,7 @@
-print("* * * * *")
-print("* * * * *")
-print("* * * * *")
-print("* * * * *")
-print("* * * * *")
+# number_1=input("input number1")
+# number_2=input("input number2")
+# # print("number1+number2=", (number_1 + number_2))
+import math
+print(math.exp(2))
+word="worJJenda"
+print(f"{word.lower()}")
