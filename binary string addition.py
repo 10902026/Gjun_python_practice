8c70a4a355998f2e7b952361f4095aec0af7e086
Jeff Li <a0983619822@gmail.com>
2023-06-06 21:16:20 +0800

    fully complete binary string addition

--- binary string addition.py
+++ binary string addition.py
@@ -1,11 +1,13 @@
+import time
+time_start = time.time()
 a=input()
 b=input()
-print(f"a={int(a,2)},b={int(b,2)}")
+# print(f"a={int(a,2)},b={int(b,2)}")
 c = int(a,2) + int(b,2)
 binary_string = ""
 while c > 0:
     remainder = c % 2
     c = c // 2
     binary_string = str(remainder) + binary_string
-    # print(f"remainder={remainder},c={c},binary_string={binary_string}")
 print(binary_string)
+print(time.time())
